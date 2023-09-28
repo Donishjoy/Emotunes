110bd34611f6f0d5b55ae5fd8872bc7cc64b561f
Abin Thomas Kurian <121152325+Abbs1912@users.noreply.github.com>
2023-09-28 18:51:40 +0530

    Update app.py
    app.py created

--- app.py
+++ app.py
@@ -12,24 +12,20 @@ import shutil
 from werkzeug.utils import secure_filename
 from flask import current_app
 from flask import send_file
+from getmail import send_mail
 import warnings
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, UserMixin, login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
 import secrets
-import re
-from flask_migrate import Migrate
-from flask_login import login_required,current_user
-
 
 warnings.filterwarnings('ignore')
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = secrets.token_hex(16)  # Generate a random secret key
 app.config["DEBUG"] = True
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///emot.db"  # Replace with your database URI
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///emo.db"  # Replace with your database URI
 db = SQLAlchemy(app)
-migrate = Migrate(app, db)
 
 # Configure Flask-Login
 login_manager = LoginManager()
@@ -38,9 +34,8 @@ login_manager.login_view = "login"
 
 # Define a user loader function
 @login_manager.user_loader
-def load_user(email):
-    # Load the user based on the email address
-    return User.query.get(email)
+def load_user(user_id):
+    return User.query.get(int(user_id))
 
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
 detect_fn = tf.saved_model.load("Models/FaceDetector/saved_model")  # Load the face detector
@@ -51,15 +46,11 @@ ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif', 'mp4'}
 static_files = ['display.css', 'eye.png', 'Picdetectb.jpg', 'thumbsup.jpg',
                 'github.png', 'IU.svg', 'UI.svg', 'RT.svg', 'UV.svg', 'VU.svg', 'feedback.svg']
 
+# User model for the database
 class User(UserMixin, db.Model):
-    email = db.Column(db.String(128), primary_key=True)  # This should be the primary key
-    username = db.Column(db.String(128), nullable=False)
-    password = db.Column(db.String(128), nullable=False)
-    def get_id(self):
-        return self.email
-
-
-
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(80), unique=True, nullable=False)
+    password = db.Column(db.String(120), nullable=False)
 
 # Create database tables
 with app.app_context():
@@ -78,11 +69,8 @@ def picdelete():
     return ("nothing")
 
 @app.route('/')
-def index():
-    if current_user.is_authenticated:
-        return redirect(url_for('dashboard'))
-    else:
-        return redirect(url_for('login'))
+def home():
+    return render_template('home.html')
 
 @app.route('/webcam')
 def webcam():
@@ -152,6 +140,43 @@ def detectandupdate(img):
 
     return [img, "pred" + img]
 
+@app.route('/detectpic', methods=['GET', 'POST'])
+@login_required  # Protect this route, requires authentication
+def detectpic():
+    UPLOAD_FOLDER = 'static'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    if request.method == 'POST':
+
+        file = request.files['file']
+
+        if file and allowed_file(file.filename):
+
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            result = detectandupdate(filename)
+            return render_template('showdetect.html', orig=result[0], pred=result[1])
+
+@app.route('/picdetect')
+@login_required  # Protect this route, requires authentication
+def picdetect():
+    return render_template('picdetect.html')
+
+@app.route('/feedback')
+@login_required  # Protect this route, requires authentication
+def feedback():
+    return render_template('feedback.html')
+
+@app.route('/sentsafe', methods=['GET', 'POST'])
+@login_required  # Protect this route, requires authentication
+def send_sentsafe():
+    if request.method == 'POST':
+        email = request.form['email']
+        comments = request.form['comments']
+        name = request.form['name']
+        comments = email + "  \n " + name + "  \n " + comments
+        send_mail(email, comments)
+    return render_template('sentfeed.html')
 
 @socketio.on("message")
 def handleMessage(input):
@@ -211,14 +236,34 @@ def detectandupdatevideo(video):
 
     return [video, "pred" + video]
 
+@app.route('/detectvideo', methods=['GET', 'POST'])
+@login_required  # Protect this route, requires authentication
+def detectvideo():
+    UPLOAD_FOLDER = 'static'
+    app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    if request.method == 'POST':
 
+        file = request.files['file']
+
+        if file and allowed_file(file.filename):
+
+            filename = secure_filename(file.filename)
+            file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+
+            result = detectandupdatevideo(filename)
+            return render_template('showvideo.html', orig=result[0], pred=result[1])
+
+@app.route('/video')
+@login_required  # Protect this route, requires authentication
+def video():
+    return render_template('video.html')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
     if request.method == 'POST':
-        email = request.form['email']
+        username = request.form['username']
         password = request.form['password']
-        user = User.query.filter_by(email=email).first()
+        user = User.query.filter_by(username=username).first()
         if user and check_password_hash(user.password, password):
             login_user(user)
             flash('Login successful!', 'success')
@@ -227,40 +272,27 @@ def login():
             flash('Login failed. Please check your credentials.', 'danger')
     return render_template('login.html')
 
-email_pattern = r'^[\w\.-]+@[\w\.-]+(\.[\w]+)+$'
-password_pattern = r'^(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$'
-
 @app.route('/register', methods=['GET', 'POST'])
 def register():
     if request.method == 'POST':
-        email=request.form['email']
         username = request.form['username']
         password = request.form['password']
-
-        # Check if the provided username is a valid email address
-        if not re.match(email_pattern, email):
-            flash('Invalid email format. Please provide a valid email address.', 'danger')
-        elif not re.match(password_pattern, password):
-            flash('Password must be at least 8 characters long and contain one capital letter, one number, and one special character.', 'danger')
+        user = User.query.filter_by(username=username).first()
+        if user:
+            flash('Username already exists. Please choose another username.', 'danger')
         else:
-            user = User.query.filter_by(email=email).first()
-            if user:
-                flash('Email already exists. Please choose another email address.', 'danger')
-            else:
-                new_user = User(email=email,username=username, password=generate_password_hash(password, method='sha256'))
-                db.session.add(new_user)
-                db.session.commit()
-                flash('Registration successful! You can now login.', 'success')
-                return redirect(url_for('login'))
+            new_user = User(username=username, password=generate_password_hash(password, method='sha256'))
+            db.session.add(new_user)
+            db.session.commit()
+            flash('Registration successful! You can now login.', 'success')
+            return redirect(url_for('login'))
     return render_template('register.html')
-
-
 
 @app.route('/dashboard')
 @login_required  # Protect this route, requires authentication
 def dashboard():
     # Add code for the dashboard page here
-    return render_template('home.html',user=current_user)
+    return render_template('home.html')
 
 @app.route('/logout')
 @login_required  # Protect this route, requires authentication
